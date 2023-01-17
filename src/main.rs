3a7b958f9877fd686bddc1d2a5c535875c911fbb
Joshua Finkelstein <joshfinkelstein3@gmail.com>
2023-01-17 11:52:05 -0500

    modularizing the architecture

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,12 @@
+use http::Method;
+use server::Server;
+use http::Request;
+
+mod server;
+mod http;
+
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run()
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr: addr
-        }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr)
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method
-}
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH
 }
