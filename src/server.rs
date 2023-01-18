ada812ff6de0222525b8a6a9a9f3204d5e66d0ea
Joshua Finkelstein <joshfinkelstein3@gmail.com>
2023-01-18 08:30:49 -0500

    starting to handle requests from our server

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 pub struct Server {
     addr: String,
@@ -21,7 +24,19 @@ impl Server {
                 match listener.accept() {
                     Ok((mut stream, addr)) => { 
                         let mut buffer = [0; 1024];
-                        stream.read(&mut buffer);
+                        match stream.read(&mut buffer) {
+                            Ok(_) => {
+                                println!("recieved a request: {}", String::from_utf8_lossy(&buffer));
+
+                                match Request::try_from(&buffer[..]) {
+                                    Ok(request) => {},
+                                    Err(e) => println!("Failed to parse a request {}", e)
+                                }
+                            
+                            }
+
+                            Err(e) => println!("Failed to read from connection: {}", e),
+                        };
                     },
                     
                     Err(e) => println!("Failed to establish a connection: {}", e)
