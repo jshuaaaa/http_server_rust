ad05adad2fa255de905e7d3ca8420ee79a413242
Joshua Finkelstein <joshfinkelstein3@gmail.com>
2023-01-18 08:36:19 -0500

    bug-fixing request.rs

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,16 +1,10 @@
-use std::method::Method;
+use super::method::Method;
 use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: super::method::Method
-}
-
-impl Request {
-    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
-
-    }
 }
 
 impl TryFrom<&[u8]> for Request {
